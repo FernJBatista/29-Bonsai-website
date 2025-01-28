249df4b6c635be3777320c5f10b59110eea12f82
Fernando Batista <fernjbatista@gmail.com>
2025-01-28 19:03:46 +0100

    Update and fixed mostly everything. Just have to adjust the plans section properly when the toggle is clicked, Also need to do mobile responsive.

--- index.js
+++ index.js
@@ -16,4 +16,45 @@ document.addEventListener("DOMContentLoaded", () => {
           }  
           );
     });
+
+    const footerToggles = document.querySelectorAll(".footer-toggle");
+
+    footerToggles.forEach((toggle) => {
+      toggle.addEventListener("click", function () {
+          this.classList.toggle("active");
+
+        // Checking the next sibling element
+        const nextSibling = this.nextElementSibling;
+        if (nextSibling?.classList.contains("footer-panel")) {
+          
+          // Access the correct panel element
+          const panel = nextSibling;
+
+          // Toggle maxheight for the panel
+          if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+          } else {
+            panel.style.maxHeight = (panel.scrollHeight)+ "px";
+          }
+        }
+    })
+});
+
+const checkbox = document.getElementById('checkbox');
+  const monthlyPlan = document.querySelector('#monthly-plans');
+  const yearlyPlan = document.querySelector('#yearly-plans');
+
+  checkbox.addEventListener('change', function() {
+    if (checkbox.checked) {
+      monthlyPlan.style.opacity = '0';
+      monthlyPlan.style.visibility = 'hidden'
+      yearlyPlan.style.opacity = '1';
+      yearlyPlan.style.visibility = 'visible'
+    } else {
+      monthlyPlan.style.opacity = '1';
+      monthlyPlan.style.visibility = 'visible'
+      yearlyPlan.style.opacity = '0';
+      yearlyPlan.style.visibility = 'hidden'
+    }
+});
 });
