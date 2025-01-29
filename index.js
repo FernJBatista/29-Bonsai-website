eee03760882533051a6ee3cbb4d2ff9666a0ed16
Fernando Batista <fernjbatista@gmail.com>
2025-01-29 17:36:47 +0100

    Basically finished. Mobile responsive is ready, mobile menu works, minimal js added too.

--- index.js
+++ index.js
@@ -41,20 +41,58 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 const checkbox = document.getElementById('checkbox');
-  const monthlyPlan = document.querySelector('#monthly-plans');
-  const yearlyPlan = document.querySelector('#yearly-plans');
+const monthlyPlan = document.querySelector('#monthly-plans');
+const yearlyPlan = document.querySelector('#yearly-plans');
 
   checkbox.addEventListener('change', function() {
     if (checkbox.checked) {
       monthlyPlan.style.opacity = '0';
       monthlyPlan.style.visibility = 'hidden'
+      monthlyPlan.style.display = 'none'
       yearlyPlan.style.opacity = '1';
       yearlyPlan.style.visibility = 'visible'
+      yearlyPlan.style.display = 'flex'
     } else {
       monthlyPlan.style.opacity = '1';
       monthlyPlan.style.visibility = 'visible'
+      monthlyPlan.style.display = 'flex'
       yearlyPlan.style.opacity = '0';
       yearlyPlan.style.visibility = 'hidden'
+      yearlyPlan.style.display = 'none'
     }
 });
+
+const hamburger = document.getElementById('hamburger-menu');
+const mobileMenu = document.getElementById('mobile-menu');
+
+hamburger.addEventListener('click', function() {
+  if (mobileMenu.style.display === 'none') {
+    mobileMenu.style.display = 'flex';
+  } else {
+    mobileMenu.style.display = 'none';
+  }
+});
+
+const closeMenu = document.getElementById('close');
+closeMenu.addEventListener('click', function() {
+  mobileMenu.style.display = 'none';
+});
+
+const mobileDropDown = document.getElementsByClassName('dropdown');
+mobileDropDown.forEach((dropdown) => {
+  addEventListener('click', function() {
+    const nextSibling = this.nextElementSibling;
+
+    if (nextSibling?.classList.contains("dropdown-section")) {
+      const panel = nextSibling;
+
+      if (panel.style.display === 'none') {
+        panel.style.display = 'flex';
+      } else {
+        panel.style.display = 'none';
+      }
+    }
+  });
+});
+
 });
